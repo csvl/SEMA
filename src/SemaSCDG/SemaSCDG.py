02c418622ffa2ba07bddc5d5da1c2c46b543d14d
ElNiak <christophe.crochet@uclouvain.be>
2023-07-07 16:52:08 +0200

    update log format for better formatting

--- src/SemaSCDG/SemaSCDG.py
+++ src/SemaSCDG/SemaSCDG.py
@@ -297,7 +297,7 @@ class SemaSCDG:
         except:
             os.makedirs(exp_dir + "/" +  nameFileShort)
         
-        fileHandler = logging.FileHandler(exp_dir + "/" + nameFileShort + "/" + "scdg.log")
+        fileHandler = logging.FileHandler(exp_dir + "/" + nameFileShort + "/" + "scdg.ans")
         fileHandler.setFormatter(CustomFormatter())
         #logging.getLogger().handlers.clear()
         try:
